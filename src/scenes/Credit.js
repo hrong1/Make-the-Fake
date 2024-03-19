0d1f049ec0993d4b2c708f4d759d1112c5000b87
hrong1 <hrong1@ucsc.edu>
2024-03-18 22:29:35 -0700

    credit

--- src/scenes/Credit.js
+++ src/scenes/Credit.js
@@ -15,7 +15,7 @@ class Credit extends Phaser.Scene {
         // grab keyboard binding from Keys scene
         let menuConfig = {
             frontFamily: 'Conrier',
-            fontSize: '48px',
+            fontSize: '24px',
             backgroundColor: '#000000',
             color: '#ffffff',
             align: 'right',
@@ -25,17 +25,26 @@ class Credit extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.rectangleA = this.add.rectangle(260, 270, 120, 60, 0xFFFFFF).setOrigin(0, 0)
-        this.rectangleB = this.add.rectangle(232, 370, 177, 60, 0xFFFFFF).setOrigin(0, 0)
-        this.rectangleC = this.add.rectangle(260, 470, 120, 60, 0xFFFFFF).setOrigin(0, 0)
-        this.rectangleA.setStrokeStyle(4, 0xffffff);
-        this.rectangleB.setStrokeStyle(4, 0xffffff);
-        this.rectangleC.setStrokeStyle(4, 0xffffff);
-        this.add.text(320, 300, 'Still', menuConfig).setOrigin(0.5)
-        this.add.text(320, 400, 'Working on', menuConfig).setOrigin(0.5)
-        this.add.text(320, 500, 'R for exit', menuConfig).setOrigin(0.5)
+        this.add.text(320, 50, 'Art by: Haorong Rong', menuConfig).setOrigin(0.5)
+        this.add.text(320, 100, 'Teaching: Nathan Altice', menuConfig).setOrigin(0.5)
+        this.add.text(320, 150, 'Sound effect by: jfxr', menuConfig).setOrigin(0.5)
+        this.add.text(320, 200, 'Music: ', menuConfig).setOrigin(0.5)
+        this.add.text(320, 250, 'Space', menuConfig).setOrigin(0.5)
+        this.add.text(320, 300, 'by Yurii Kohut from Pixabay', menuConfig).setOrigin(0.5)
+        this.add.text(320, 400, 'Space Atmospheric Background', menuConfig).setOrigin(0.5)
+        this.add.text(320, 450, 'by Music Unlimited from Pixabay', menuConfig).setOrigin(0.5)
+        this.add.text(320, 550, 'Feel This Energy (Voyager)', menuConfig).setOrigin(0.5)
+        this.add.text(320, 600, 'by AlexGrohl from Pixabay', menuConfig).setOrigin(0.5)
+
         // define keys
         keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        // pointer down
+        this.input.on('pointerdown', () =>
+            {
+                this.scene.stop("creditScene")
+                this.scene.stop("playScene")
+                this.scene.launch('menuScene')
+            })
     }
 
     update() {
